eac39669f530008acc7721cc02c39035894b40fe
nabilaarab <nabilaarablyon@gmail.com>
2026-04-26 21:59:19 +0200

    Init  structure

---
+++ linkedin-outreach/LinkedinOutReach/LinkedinOutReach/ExcelManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedinOutReach
+{
+    internal class ExcelManager
+    {
+        private string filePath { get; set; }
+        private string[] linkedinProfiles { get; }
+
+        public void loadLinkedinProfiles()
+        {
+            
+        }
+    }
+}
---
+++ linkedin-outreach/LinkedinOutReach/LinkedinOutReach/LinkedinProfile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedinOutReach
+{
+    public class LinkedinProfile
+    {
+        public string Id { get; }
+        public string Link { get; }
+        public string Name { get; }
+        public string CompanyName { get; }
+
+        public LinkedinProfile(
+            string id, 
+            string link, 
+            string name, 
+            string companyName
+        ){
+            Id = id;
+            Link = link;
+            Name = name;
+            CompanyName = companyName;
+        }
+    }
+}
