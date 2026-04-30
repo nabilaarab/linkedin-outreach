f00275589ad759cb387d098737b4c00c0c3c43b5
nabilaarab <nabilaarablyon@gmail.com>
2026-04-30 23:56:12 +0200

    Implement BrowserDriverActionNewProfilesExtraction class

---
+++ linkedin-outreach/LinkedinOutReach/LinkedinOutReach/browserDriverAction/BrowserDriverActionNewProfilesExtraction.cs
@@ -0,0 +1,31 @@
+﻿using LinkedinOutReach.models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedinOutReach.browserDriverAction
+{
+    internal class BrowserDriverActionNewProfilesExtraction : BrowserDriverAction
+    {
+        public BrowserDriverActionNewProfilesExtraction(BrowserDriver browserDriver) : base(browserDriver) { }
+        public override async Task Run(LinkedinProfile linkedinProfile)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                this.goToPeopleSearchPage("IT Manager", i);
+                Task.Delay(Random.Shared.Next(5000, 6000)).Wait();
+            }
+        }
+
+        private void goToPeopleSearchPage(string keyword, int pageNumber)
+        {
+            string url = $"https://www.linkedin.com/search/results/people/?keywords={keyword}&page={pageNumber}";
+            this.goToPage(url);
+        }
+
+        public override string ToString()
+        {
+            return "Action - Extraction de nouveaux profils";
+        }
+    }
+}
