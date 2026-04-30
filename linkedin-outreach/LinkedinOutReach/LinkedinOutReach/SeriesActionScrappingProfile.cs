5efc1ebec500f23b9b4a7a5ad01b6dfa8c35b145
nabilaarab <nabilaarablyon@gmail.com>
2026-04-30 23:40:20 +0200

    Add SeriesActionScrappingProfile

---
+++ linkedin-outreach/LinkedinOutReach/LinkedinOutReach/SeriesActionScrappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using LinkedinOutReach.browserDriverAction;
+using LinkedinOutReach.models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedinOutReach
+{
+    internal class SeriesActionScrappingProfile : SeriesAction
+    {
+        public SeriesActionScrappingProfile(BrowserDriver browserDriver, LinkedinProfile[] linkedinProfiles, List<BrowserDriverAction> actionDesired, int minTimeBetweenActions, int maxTimeBetweenActions) : 
+            base(browserDriver, linkedinProfiles, actionDesired, minTimeBetweenActions, maxTimeBetweenActions) {}
+
+        public override void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
